8c00cb047821b3af6f5366a2deca7652c2b0c53b
Gimachombe <jacobgimachombe@gmail.com>
2024-10-17 14:55:49 +0300

    docs: add backend documentation
    - add `README` file
    - create instructions on how to setup the application
    - document endpoint responses

--- backend/app/v1/views/meetings.py
+++ backend/app/v1/views/meetings.py
@@ -1,8 +1,6 @@
 import json
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
 from ..models import Meeting, Report
-from utils.exception import DatabaseException
-from utils.responses import response, response_with_data
 
 
 meetings_blueprint = Blueprint("meetings_blueprint", __name__)
@@ -14,23 +12,14 @@ def create():
 
     try:
         data = request.get_json()
-        required_fields = [
-            "title",
-            "description",
-            "start_time",
-            "end_time",
-            "organization_id",
-            "location",
-            "meeting_date",
-            "boardroom_id",
-        ]
-        if not data or not isinstance(data, dict):
-            return response("Invalid JSON format or empty payload", 400)
+        # resources_json = json.dumps(data["resources_id"])
+        # if not data or not isinstance(data, dict):
+        #     return jsonify({"msg": "Invalid JSON format or empty payload"}), 400
+        required_fields = ["title", "description", "start_time", "end_time"]
         missing_fields = [field for field in required_fields if field not in data]
         if missing_fields:
-            return response(f"Missing required fields: {', '.join(missing_fields)}", 400)
-
-        result = meeting.create(
+            return jsonify({"msg": f"Missing required fields: {', '.join(missing_fields)}"}), 400
+        meeting.create(
             data.get("title"),
             data.get("description"),
             data.get("meeting_date"),
@@ -38,26 +27,28 @@ def create():
             data.get("end_time"),
             data.get("boardroom_id"),
             data.get("organization_id"),
-            data.get("location"),
+            # resources_json,
+            # data.get("location"),
+            'Nyayo House'
         )
-        if not isinstance(result, Exception):
-            return response("Meeting added successfully!", 201)
-        else:
-            raise DatabaseException(str(result))
-    except DatabaseException as e:
-        return response("Something went wrong, " + str(e), 400)
+        return jsonify({"msg": "Meeting added successfully"}), 201
+    except Exception as e:
+
+        return jsonify({
+            "msg":"Something went wrong, "+str(e),
+        }),500
 
 
 @meetings_blueprint.route("/api/v1/meetings", methods=["GET"])
 def fetchall():
     meetings = Meeting()
-    return response_with_data("OK", meetings.get_all(), 200)
+    return jsonify(meetings.get_all())
 
 
 @meetings_blueprint.route("/api/v1/meetings/<int:id>", methods=["GET"])
 def fetchone(id):
     meetings = Meeting()
-    return response_with_data("OK", meetings.get_by_id(id), 200)
+    return jsonify(meetings.get_by_id(id))
 
 
 @meetings_blueprint.route("/api/v1/meetings/<int:meeting_id>", methods=["PUT"])
@@ -74,24 +65,21 @@ def update(meeting_id):
         end_time = data["end_time"]
         boardroom_id = data["boardroom_id"]
 
-        if not data or not isinstance(data, dict):
-            return response("Invalid JSON format or empty payload", 400)
-
         # Check if all necessary fields are provided
         if not all([title, description, meeting_date, start_time, end_time, boardroom_id]):
-            return response("Missing required fields!", 400)
+            return jsonify({"error": "Missing required fields"}), 400
 
         if not meeting.get_by_id(meeting_id):
-            return response("Meeting not found!", 404)
+            return jsonify({"error": "Meeting not found"}), 404
 
         meeting.update(
             meeting_id, title, description, meeting_date, start_time, end_time, boardroom_id
         )
 
-        return response("Meeting updated successfully!", 200)
+        return jsonify({"message": "Meeting updated successfully"}), 200
 
-    except DatabaseException as e:
-        return response("Something went wrong, " + str(e), 400)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @meetings_blueprint.route("/api/v1/meetings/update-status/<int:meeting_id>", methods=["PATCH"])
@@ -103,14 +91,15 @@ def update_status(meeting_id):
 
         # Check if all necessary fields are provided
         if not all([status]):
-            return response("Missing required fields!", 400)
+            return jsonify({"error": "Missing required fields"}), 400
 
         if not meeting.get_by_id(meeting_id):
-            return response("Meeting not found!", 404)
+            print(meeting.get_by_id(meeting_id))
+            return jsonify({"error": "Meeting not found"}), 404
         meeting.update_status(meeting_id, status)
-        return response("Meeting status updated successfully!", 200)
-    except DatabaseException as e:
-        return response("Something went wrong, " + str(e), 400)
+        return jsonify({"message": "Meeting status updated successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @meetings_blueprint.route("/api/v1/meetings/<int:id>", methods=["DELETE"])
@@ -118,17 +107,17 @@ def delete(id):
     meeting = Meeting()
     try:
         if not meeting.get_by_id(id):
-            return response("Meeting not found!", 404)
+            return jsonify({"error": "Meeting not found"}), 404
         meeting.delete(id)
-        return response("Meeting deleted successfully!", 200)
-    except DatabaseException as e:
-        return response("Something went wrong, " + str(e), 400)
+        return jsonify({"msg": "Meeting deleted successfully"}), 200
+    except Exception as e:
+        return jsonify({"msg": f"Error occurred: {str(e)}"}), 500
 
 
 @meetings_blueprint.route("/api/v1/meetings/summary", methods=["GET"])
 def summerize():
     report = Report()
     try:
-        return response_with_data("OK", report.meetings_summary(), 200)
-    except DatabaseException as e:
-        return response("Something went wrong, " + str(e), 400)
+        return jsonify(report.meetings_summary())
+    except Exception as e:
+        return jsonify({"msg": f"Error occurred: {str(e)}"}), 500
